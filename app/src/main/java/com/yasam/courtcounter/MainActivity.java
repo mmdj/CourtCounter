99cbfc4f848749ec88d5b0933e787db2bf990b1f
telranAndroid <telran.asana@gmail.com>
2016-04-21 02:22:34 +0300

    022_Setup_The_Methods_Buttons_Click_TeamA

--- app/src/main/java/com/yasam/courtcounter/MainActivity.java
+++ app/src/main/java/com/yasam/courtcounter/MainActivity.java
@@ -2,6 +2,7 @@ package com.yasam.courtcounter;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,7 +12,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        displayForTeamA(8);
+        displayForTeamA(0);
     }
 
     /**
@@ -22,5 +23,29 @@ public class MainActivity extends AppCompatActivity {
         TextView scoreTeamA = (TextView) findViewById(R.id.txtVw_scoreTeamA);
         if(scoreTeamA!=null)
             scoreTeamA.setText(String.valueOf(score));
+    }
+
+    /**
+     * Increases the score for Team A by 3 points.
+     */
+    public void addThreeForTeamA(View view) {
+        int step = 3;
+        displayForTeamA(step);
+    }
+
+    /**
+     * Increases the score for Team A by 2 points.
+     */
+    public void addTwoForTeamA(View view) {
+        int step = 2;
+        displayForTeamA(step);
+    }
+
+    /**
+     * Increases the score for Team A by 1 point.
+     */
+    public void addOneForTeamA(View view) {
+        int step = 1;
+        displayForTeamA(step);
     }
 }
