bed34556358a494af48159f39353794b52a249ed
telranAndroid <telran.asana@gmail.com>
2016-04-21 04:13:37 +0300

    061_Add_The_Other_Team_In_Java

--- app/src/main/java/com/yasam/courtcounter/MainActivity.java
+++ app/src/main/java/com/yasam/courtcounter/MainActivity.java
@@ -10,6 +10,7 @@ public class MainActivity extends AppCompatActivity {
     private final int POINTS_FOR_FREE_THROW = 1;
 
     private int mScoreTeamA = 0;
+    private int mScoreTeamB = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +18,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         displayForTeamA(0);
+        displayForTeamB(0);
     }
 
     /**
@@ -27,6 +29,15 @@ public class MainActivity extends AppCompatActivity {
         TextView scoreTeamA = (TextView) findViewById(R.id.txtVw_scoreTeamA);
         if(scoreTeamA!=null)
             scoreTeamA.setText(String.valueOf(score));
+    }
+    /**
+     * Displays the given score for Team B
+     * @param score - given score for Team A to display
+     */
+    private void displayForTeamB(int score){
+        TextView scoreTeamB = (TextView) findViewById(R.id.txtVw_scoreTeamB);
+        if(scoreTeamB!=null)
+            scoreTeamB.setText(String.valueOf(score));
     }
 
     /**
@@ -54,5 +65,33 @@ public class MainActivity extends AppCompatActivity {
         int step = POINTS_FOR_FREE_THROW;
         mScoreTeamA += step;
         displayForTeamA(mScoreTeamA);
+    }
+
+
+    /**
+     * Increases the score for Team A by 3 points.
+     */
+    public void addThreeForTeamB(View view) {
+        int step = 3;
+        mScoreTeamB += step;
+        displayForTeamB(mScoreTeamB);
+    }
+
+    /**
+     * Increases the score for Team A by 2 points.
+     */
+    public void addTwoForTeamB(View view) {
+        int step = 2;
+        mScoreTeamB += step;
+        displayForTeamB(mScoreTeamB);
+    }
+
+    /**
+     * Increases the score for Team A by 1 point.
+     */
+    public void addOneForTeamB(View view) {
+        int step = POINTS_FOR_FREE_THROW;
+        mScoreTeamB += step;
+        displayForTeamB(mScoreTeamB);
     }
 }
